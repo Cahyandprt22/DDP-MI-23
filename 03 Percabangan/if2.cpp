e6b33623f57e9aa9a69e782a9c0acd98d92587aa
Cahyandprt22 <143464656+Cahyandprt22@users.noreply.github.com>
2023-10-01 11:10:59 +0700

    Update if2.cpp

--- 03 Percabangan/if2.cpp
+++ 03 Percabangan/if2.cpp
@@ -2,18 +2,14 @@
 
 using namespace std;
 
-int main()
-{
+int main() {
     int x;
 
     cin >> x;
 
-    if (x == 100)
-    {
+    if (x == 100) {
         cout << "SEMPURNA" << endl;
-    }
-    else
-     {
+    } else {
         cout << "BELUM SEMPURNA" << endl;
     }
 }
