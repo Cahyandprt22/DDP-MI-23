4f2f85cc67f78553a1e0b72b56a6da7f91f382c9
Cahyandprt22 <nailacahya580gmail.com>
2023-10-06 18:38:59 +0700

    Tugas 4 DDP

--- 03 Percabangan/if2.cpp
+++ 03 Percabangan/if2.cpp
@@ -2,20 +2,23 @@
 
 using namespace std;
 
-int main() {
+int main()
+{
     int x;
 
     cin >> x;
 
-    if (x == 100) {
+    if (x == 100)
+    {
         cout << "SEMPURNA" << endl;
-<<<<<<< HEAD
     }
     else
     {
-=======
-    } else {
->>>>>>> e6b33623f57e9aa9a69e782a9c0acd98d92587aa
+        == == == =
+    }
+    else
+    {
+
         cout << "BELUM SEMPURNA" << endl;
     }
 }
