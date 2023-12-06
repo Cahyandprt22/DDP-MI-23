f81c28c06ac03c389380ca42a9e136fdd3b829bb
Cahyandprt22 <nailacahya580gmail.com>
2023-12-06 18:49:41 +0700

    07 shorting

---
+++ 07 shorting/swap.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 10;
+    int b = 5;
+
+    int temp;
+    temp = a;
+    b = a;
+
+    cout << a << endl;
+    cout << b << endl;
+}
